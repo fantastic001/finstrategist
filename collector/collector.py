9d8879c0cf318cb62330269943e60ef48bae1a53
Stefan Nožinić <stefan@lugons.org>
2020-05-31 12:37:40 +0200

    update collector implementation

--- collector/collector.py
+++ collector/collector.py
@@ -1,12 +1,22 @@
-from yahoo_finance import Share 
 import yfinance as yf
+import requests
+import os
 
-
-
-shares = [
-    "AAPL",
-]
+shares  = os.getenv("STOCKS").split(" ")
+endpoint = os.getenv("WEB_HOOK")
 
 for share in shares:
+    print(share)
     s = yf.Ticker(share)
+    # print(s.history("1mo"))
     print(s.info)
+    data = {
+        "close": s.info["previousClose"],
+        "open": s.info["regularMarketOpen"],
+        "high": s.info["dayHigh"],
+        "low": s.info["dayLow"], 
+        "volume": s.info["regularMarketVolume"],
+        "dividend": s.info["dividendRate"],
+    }
+    print(data)
+    requests.post(endpoint, data)
